df35b3cca24dbe058ef5d3acc0e047e8379dc603
Phumrapee Soenvanichakul <77825705+Jannnn1235@users.noreply.github.com>
2021-11-01 11:24:09 +0700

    Update main.py

--- timetable/main.py
+++ timetable/main.py
@@ -22,7 +22,7 @@ async def talk_bot(message):
     day_today = str(date.today().strftime("%A"))
     time_start = date.today().strftime("%H:%M")  
 
-    def findX(time_start):
+    def findX():
         for i in range(11):
             if str(time_start) >= table.timestart['timestart'][i] and str(time_start) <= table.timestart['timestart'][i+1]:
                 return i
@@ -30,7 +30,7 @@ async def talk_bot(message):
                 return i
 
     if 'คาบ' in message.content:
-        x = findX(time_start)
+        x = findX()
         if 'คาบนี้' in message.content:  
             if day_today in table.day and x < 10:
                 await message.channel.send(f"{table.day[f'{day_today}'][x]} {table.timestart['timestart'][x]} - {table.timestart['timestart'][x+1]}")
@@ -49,17 +49,17 @@ async def talk_bot(message):
             else:
                 await message.channel.send("ไม่มีเรียนไอ้สัสเอ้ย อย่าติดตลก")
 
-        if 'ตารางเรียน' in message.content:   
-            file = discord.File("image/ตารางเรียน.jpg")
-            await message.channel.send(file = file)
+    if 'ตารางเรียน' in message.content:   
+        file = discord.File("image/ตารางเรียน.jpg")
+        await message.channel.send(file = file)
 
     if message.content == 'n.help':
-        embed=discord.Embed(title="ช่วยเหลือ" , color=0x84c5e6)
+        embed=discord.Embed(title="ช่วยเหลือ" , color=blue)
         embed.add_field(name="คาบนี้", value="พิมพ์ในช่องแชท", inline=False)
         embed.add_field(name="คาบต่อไป", value="พิมพ์ในช่องแชท", inline=False)
         embed.add_field(name="ตารางเรียน", value="พิมพ์ในช่องแชท", inline=True)
         embed.set_thumbnail(url = bot.user.avatar_url)
-        embed.set_footer(text="github : https://github.com/Jannnn1235/NENEbot")
+        embed.set_footer(text=github)
         await message.channel.send(embed=embed)
         
 bot.run("#HERE")
